251d0fc4b84c08090abb2bb98774e60cb4ef3567
Tim Broder <timothy.broder@gmail.com>
2011-10-02 19:30:14 -0400

    wordpress update

--- contrib/wordpress.rb
+++ contrib/wordpress.rb
@@ -13,6 +13,18 @@ require 'yaml'
 
 module Jekyll
   module WordPress
+
+    # Reads a MySQL database via Sequel and creates a post file for each
+    # post in wp_posts that has post_status = 'publish'.
+    # This restriction is made because 'draft' posts are not guaranteed to
+    # have valid dates.
+    QUERY = "select post_title, post_name, post_date, post_content, post_excerpt, ID, guid from wp_posts where post_status = 'publish' and post_type = 'post'"
+	# Fetch all tags for a given POST ID
+	TAGS_QUERY = "select tm.term_id,tm.name from wp_term_relationships tr 
+				  inner join wp_term_taxonomy tt on tr.term_taxonomy_id = tt.term_taxonomy_id 
+				  inner join wp_terms tm on tm.term_id=tt.term_id  
+				  where tr.object_id=%d and tt.taxonomy = 'post_tag'";
+				  
     def self.process()
       gem('mysql')
       puts 'init'
@@ -23,36 +35,54 @@ module Jekyll
       # Reads a MySQL database via Sequel and creates a post file for each
       # post in wp_posts that has post_status = 'publish'. This restriction is
       # made because 'draft' posts are not guaranteed to have valid dates.
-      query = "select post_name, post_date, post_content, post_excerpt, id, guid from wp_posts where post_status = 'publish' and post_type = 'post' order by id limit 3"
+      query = "select post_name, post_title, post_date, post_content, post_excerpt, id, guid from wp_posts where post_status = 'publish' and post_type = 'post' order by id limit 1"
 
-      db[query].each do |post|
-        # Get required fields and construct Jekyll compatible name.
-        puts "!"
-        title = post[:post_title]
-        slug = post[:post_name]
-        date = post[:post_date]
-        content = post[:post_content]
-        name = "%02d-%02d-%02d-%s.markdown" % [date.year, date.month, date.day,
-                                               slug]
+            db[QUERY].each do |post|
+              # Get required fields and construct Jekyll compatible name
+              title = post[:post_title]
+              slug = post[:post_name]
+              date = post[:post_date]
+              content = post[:post_content]
+              name = "%02d-%02d-%02d-%s.markdown" % [date.year, date.month, date.day, slug]
 
-        # Get the relevant fields as a hash, delete empty fields and convert
-        # to YAML for the header.
-        data = {
-           'layout' => 'post',
-           'title' => title.to_s,
-           'excerpt' => post[:post_excerpt].to_s,
-           'wordpress_id' => post[:ID],
-           'wordpress_url' => post[:guid],
-           'date' => date
-         }.delete_if { |k,v| v.nil? || v == '' }.to_yaml
+      		# Get associated taxonomy terms (tags)
+      		# We replace + with nothing and transform to lower case
+      		# TODO: figure out what other characters would fuck up YAML
+      		tags = []
+      		db[TAGS_QUERY % post[:ID]].each do |tag|
+      			tags << tag[:name].to_s.gsub('+','').downcase
+      		end
 
-        # Write out the data and content to file
-        File.open("../source/_posts/#{name}", "w") do |f|
-          f.puts data
-          f.puts "---"
-          f.puts content
+      		# Process content to rewrite some URLs
+      		#if domain
+      		#        content = self.transformUrls(domain,content)
+      		#end
+
+              # Get the relevant fields as a hash, delete empty fields and convert
+              # to YAML for the header
+              data = {
+                 'layout' => 'post',
+                 'title' => title.to_s,
+                 'excerpt' => post[:post_excerpt].to_s,
+      		   'tags' => tags
+               }.delete_if { |k,v| v.nil? || v == ''}.to_yaml
+
+              # Write out the data and content to file
+              File.open("../source/_posts/#{name}", "w") do |f|
+                f.puts data
+                f.puts "---"
+                f.puts content
+              end
+            end
+
+          end
+
+      	# Process the content and replace URLs pointing to wp-content/uploads/
+      	# with CloudFront CNAME'd URL turbo.paulstamatiou.com/uploads/
+      	#def self.transformUrls(domain,content)
+      	#	baseurl = "%s/wp-content/uploads/" % domain
+      	#	return content.gsub(baseurl,"turbo.paulstamatiou.com/uploads/")
+      	#end
+
         end
       end
-    end
-  end
-end
