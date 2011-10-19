fbe74a1808a4a27159607ca7ab6b0a067e9476e7
Tim Broder <timothy.broder@gmail.com>
2011-10-18 21:13:26 -0400

    git commit hook

---
+++ contrib/commit-hook/rake-generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+
+cd /code/octopress
+pwd
+rake generate
